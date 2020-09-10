af878c5b25bd675878c7662e65e71fb565c3e617
7-- <regop412@gmail.com>
2020-09-09 23:24:41 -0400

    show images. make ready for frontend design

---
+++ README.txt
@@ -0,0 +1,6 @@
+HOW TO RUN
+1. Install Eclipse and add Spring Tool Suite extension
+2. Clone the project from Git 
+3. Import project into Eclipse
+4. Right click the project and Run As -> Spring Boot App
+5. Should be deployed to http://localhost:8446/ and should update on save
---
+++ src/main/java/finance/cartel/Application.java
@@ -0,0 +1,32 @@
+package finance.cartel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+	
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {      
+	    @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/**")
+	        .addResourceLocations("classpath:/css/", "classpath:/js/", "classpath:/image/")
+	        .setCachePeriod(0);
+	    }
+	}
+}
